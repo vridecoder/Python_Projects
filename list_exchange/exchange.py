7d354cd125322349270b79c9d8d7c11a17325230
Vrinda <vri.s.ch9@gmail.com>
2020-04-22 10:42:44 +0530

    List

---
+++ list_exchange/exchange.py
@@ -0,0 +1,9 @@
+class Ex:
+    def __init__def(self,l):
+        self.l=l
+    def swap(self,list):
+        self.l=list[-1], list[0]
+        list[0],list[-1]=self.l
+        return list
+
+__all__ = ['Ex']
---
+++ list_exchange/main.py
@@ -0,0 +1,6 @@
+from exchange import Ex
+#if __name__ == 'main':
+newlist=[12,35,9,65,24]
+exchange=Ex()
+#exchange.swap(newlist)
+print(exchange.swap(newlist))
